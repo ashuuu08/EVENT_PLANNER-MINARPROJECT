0d773393003752bf95127502d1c93e75bd0c7415
Ashish Kumar Rathour <101181578+ashuuu08@users.noreply.github.com>
2024-07-09 16:25:54 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
-  mongoose.connect("mongodb+srv://rajpootashishd:ashu0000@cluster0.klwxskr.mongodb.net/?retryWrites=true",{dbName:"EVENT_MESSAGE"}).then(
+  mongoose.connect("mongodb+srv://@cluster0.klwxskr.mongodb.net/?retryWrites=true",{dbName:"EVENT_MESSAGE"}).then(
     ()=>{
       console.log("Connect to the database")
 }).catch(err=>{
